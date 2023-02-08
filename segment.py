f0ba94842ae902ec41ceb262284f140a11a75d4f
mark1cw <67136269+mark1cw@users.noreply.github.com>
2023-02-07 22:48:11 -0800

    tried to improve accuracy, bit messy, lots of commented out stuff

--- segment.py
+++ segment.py
@@ -7,6 +7,14 @@ from generate_ocr import input_size
 
 
 def check_dark_background(input_img):
+    # image grayscale and filtering
+    image = cv2.imread(input_img)
+
+    # another section code to try out
+    # gray = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+    # thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,9,3)
+    # cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
     avg_color_row = np.average(input_img, axis=0)
     avg_color = np.average(avg_color_row, axis=0)
@@ -42,12 +50,23 @@ def pad_resize(orig_image):
 def letters_extract(gray_img):
 
     # Check if the image has a black or white background
-    if check_dark_background(gray_img):
-        gray_img = cv2.bitwise_not(gray_img)
+    #if check_dark_background(gray_img):
+    #    gray_img = cv2.bitwise_not(gray_img)
+
+    # adjust kernal sizes to rid of smaller contour edges(play around with this to get optimal results)
+    kernal1 = 15
+    kernal2 = 31
+    blur1 = cv2.GaussianBlur(image, (kernal1, kernal1), 0)
+    blur2 = cv2.GaussianBlur(image, (kernal1, kernal2), 0)
+    finalblur = blur1 - blur2
+    #to ensure the output is good enough to distinguish text from background
+    cv2.imshow('Difference of Gaussians', finalblur)
+    thresh = cv2.adaptiveThreshold(finalblur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 9, 3)
+    cv2.imshow('after thresh', thresh)
 
     # Apply blur and threshold filter to help finding characters
-    blur = cv2.bilateralFilter(gray_img, 9, 75, 75)
-    ret, thresh = cv2.threshold(blur, 190, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    #blur = cv2.bilateralFilter(gray_img, 9, 75, 75)
+    #ret, thresh = cv2.threshold(blur, 190, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
     # Use findContours to get locations of characters
     cnts, heirs = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
