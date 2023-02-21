9558298e6563dbcc1c1e316c7e67adbd4679b6a7
Bryce Melander <brycemelander@gmail.com>
2023-02-21 08:52:39 -0800

    updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os.path
 import keras
 import cv2
 import numpy as np
+import time
 
 import matplotlib.pyplot as plt
 
@@ -27,7 +28,63 @@ def predict(ocr_model, image):
             break
 
 
-def main(new_model=False, epochs=12):
+def camera_input():
+
+    # User input flags
+    q_flag = 0
+    c_flag = 0
+
+    # Refresh rate in seconds
+    refresh_rate = 1
+
+    # Define camera input
+    vid = cv2.VideoCapture(0)
+
+    # Display the camera output at the specified framerate
+    while True:
+        # Refresh rate calculation
+        t_end = time.time() + refresh_rate
+
+        # Capture a video frame
+        ret, frame = vid.read()
+
+        # Convert to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+
+        # Trigger display
+        cv2.waitKey(1)
+
+        # Loop to wait for next refresh
+        while time.time() < t_end:
+
+            # Quit button
+            if 0xFF == ord('q'):
+                q_flag = 1
+                break
+
+            # Capture button
+            if 0xFF == ord('c'):
+                c_flag = 1
+                break
+
+        if q_flag or c_flag:
+            break
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+    if c_flag:
+        return frame
+
+    return 0
+
+
+def main(new_model=False, epochs=12, camera=False):
 
     # Directory containing a pre-generated model
     model_dir = "models/ocr"
@@ -47,20 +104,27 @@ def main(new_model=False, epochs=12):
     ocr_model = keras.models.load_model(filepath=model_dir)
     print("Model loaded!\n")
 
-    # Images to test
-    img_dir_arr = ["performance.png"]
+    if camera:
+        test_image = camera_input()
+        if test_image == 0:
+            print("Quit input detected, Goodbye!")
+            return 0
+        else:
+            print("Capture received!")
+    else:
+        # Image to test
+        img = "performance.png"
 
-    # Test the network
-    print("Testing model...")
-    for img in img_dir_arr:
-        print(f"Testing {img}...")
+        # Test the network
+        print("Testing model using an image file...")
 
         # Load an image in grayscale format
         test_image = cv2.imread("./test_images/" + img, cv2.IMREAD_GRAYSCALE)
+        print(f"{img} Loaded!")
 
-        # Use model to predict the contents of the image
-        predict(ocr_model, test_image)
+    # Use model to predict the contents of the image
+    predict(ocr_model, test_image)
 
 
 if __name__ == "__main__":
-    main()
+    main(camera=True)
