d7ad2ab761cf625625e0ba9469c12ff7a870c954
Bryce Melander <brycemelander@gmail.com>
2023-02-28 17:30:00 -0800

    updated

--- segment_testing.py
+++ segment_testing.py
@@ -1,8 +1,16 @@
+import time
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
 import segment
+
+# Indexing variables
+t = 0
+b = 1
+x = 0
+y = 1
 
 
 def test_letters_extract(gray_img):
@@ -37,16 +45,16 @@ def test_letters_extract(gray_img):
     # Sort the contours by box location (sorted top left to bottom right)
     # bxs = [cv2.boundingRect(c) for c in cnts]
     bxs = [cv2.minAreaRect(c) for c in cnts]
-    bxs2 = [cv2.boxPoints(b) for b in bxs]
+    bxs2 = [cv2.boxPoints(b1) for b1 in bxs]
     bxs3 = [np.int0(b2) for b2 in bxs2]
 
-    contours, boxes, hierarchies = zip(*sorted(zip(cnts, bxs3, heirs[0]), key=lambda b: b[1], reverse=False))
+    contours, boxes, hierarchies = zip(*sorted(zip(cnts, bxs3, heirs[0]), key=lambda bx: bx[1], reverse=False))
 
     # Iterate through the list of sorted contours
     letters = []
     for idx, box in enumerate(boxes):
 
-        (x, y, w, h) = box
+        (x_val, y_val, w, h) = box
 
         # Skip aspect ratios that cannot be scaled to 28x28 properly
         if (w / segment.img_resize) > h or (h / segment.img_resize) > w:
@@ -55,7 +63,7 @@ def test_letters_extract(gray_img):
         # If a contour has a child, assume it's a letter
         if hierarchies[idx][3] != -1:
             # Crop each bounding box
-            letter_crop = thresh[y:y + h, x:x + w]
+            letter_crop = thresh[y_val:y_val + h, x_val:x_val + w]
 
             # Skip blank boxes
             if np.min(letter_crop) == 255 or np.max(letter_crop) == 0:
@@ -88,13 +96,43 @@ def fit(gray_img):
     largest_cnt = sorted(contours, key=cv2.contourArea, reverse=True)[0]
 
     # get bounding box
-    x, y, w, h = cv2.boundingRect(largest_cnt)
+    x_val, y_val, w, h = cv2.boundingRect(largest_cnt)
 
     # crop result
-    return gray_img[y:y + h, x:x + w]
+    return gray_img[y_val:y_val + h, x_val:x_val + w]
+
+
+def is_intersecting(box0, box_list):
+
+    for idx, box1 in enumerate(box_list):
+
+        # Check if box1 is within box0
+        if (box0[t][x] <= box1[t][x] <= box0[b][x] and box0[t][y] <= box1[t][y] <= box0[b][y]) \
+                or (box0[t][x] <= box1[b][x] <= box0[b][x] and box0[t][y] <= box1[b][y] <= box0[b][y]) \
+                or (box0[t][x] <= box1[b][x] <= box0[b][x] and box0[t][y] <= box1[t][y] <= box0[b][y]) \
+                or (box0[t][x] <= box1[t][x] <= box0[b][x] and box0[t][y] <= box1[b][y] <= box0[b][y]):
+            return idx
+
+        # Check if box0 is within box1
+        if (box1[t][x] <= box0[t][x] <= box1[b][x] and box1[t][y] <= box0[t][y] <= box1[b][y]) \
+                or (box1[t][x] <= box0[b][x] <= box1[b][x] and box1[t][y] <= box0[b][y] <= box1[b][y]) \
+                or (box1[t][x] <= box0[b][x] <= box1[b][x] and box1[t][y] <= box0[t][y] <= box1[b][y]) \
+                or (box1[t][x] <= box0[t][x] <= box1[b][x] and box1[t][y] <= box0[b][y] <= box1[b][y]):
+            return idx
+
+    return False
+
+
+def combine(box0, box1):
+
+    x_vals = box0[t][x], box0[b][x], box1[t][x], box1[b][x]
+    y_vals = box0[t][y], box0[b][y], box1[t][y], box1[b][y]
+
+    return (np.min(x_vals), np.min(y_vals)), (np.max(x_vals), np.max(y_vals))
 
 
 def segmentation_test(gray_img):
+    t0 = time.time()
     plt.imshow(gray_img, cmap='gray')
     plt.title("Original Image")
     plt.show()
@@ -103,7 +141,7 @@ def segmentation_test(gray_img):
 
     blured = cv2.medianBlur(cropped_img, 11)
 
-    adapt_thresh = cv2.adaptiveThreshold(blured, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 7)
+    adapt_thresh = cv2.adaptiveThreshold(blured, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 7)
 
     plt.imshow(blured, cmap='gray')
     plt.title("blured")
@@ -115,27 +153,38 @@ def segmentation_test(gray_img):
     cnts, heirs = cv2.findContours(adapt_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     heirs = heirs[0]
 
-    boxes = []
-    rot_boxes = []
+    box_list = []
+    cpy_img = adapt_thresh.copy()
     for i, c in enumerate(cnts):
         if heirs[i][3] != -1:
-            rect = cv2.minAreaRect(c)
-            rot_boxes.append(np.intp(cv2.boxPoints(rect)))
 
-            x, y, w, h = cv2.boundingRect(c)
-            boxes.append(np.array([(x, y + h), (x, y), (x + w, y), (x + w, y + h)]))
+            x_val, y_val, w, h = cv2.boundingRect(c)
 
-    boxes_img = cv2.drawContours(adapt_thresh.copy(), boxes, -1, (127, 127, 127), 10)
-    plt.imshow(boxes_img, cmap='brg')
+            if (w / segment.img_resize) > h or (h / segment.img_resize) > w:
+                continue
+
+            crop = adapt_thresh[y_val:y_val + h, x_val:x_val + w]
+
+            # Skip blank boxes
+            if np.min(crop) == 255 or np.max(crop) == 0:
+                continue
+
+            # Check for overlapping boxes and combine them
+            box = (x_val, y_val), (x_val + w, y_val + h)
+            inter = is_intersecting(box, box_list)
+            if inter is False:
+                box_list.append(box)
+            else:
+                box = combine(box_list[inter], box)
+                box_list[inter] = box
+
+    for bx in box_list:
+        cv2.rectangle(cpy_img, bx[0], bx[1], (127, 127, 127), thickness=4)
+    plt.imshow(cpy_img, cmap='brg')
     plt.title("Final Image")
     plt.show()
 
-    rot_img = cv2.drawContours(adapt_thresh.copy(), rot_boxes, -1, (127, 127, 127), 10)
-    plt.imshow(rot_img, cmap='brg')
-    plt.title("Final Image with Rotated Boxes")
-    plt.show()
-
-    print('')
+    print(f"Total time: {time.time() - t0:.2f} seconds")
 
 
 if __name__ == "__main__":
