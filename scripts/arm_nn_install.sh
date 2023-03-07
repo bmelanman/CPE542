6c6a332f7c6b170d873f5fca1f1fa3dc5ce0cbe6
Bryce Melander <brycemelander@gmail.com>
2023-03-06 21:44:55 -0800

    ˆ

--- scripts/arm_nn_install.sh
+++ scripts/arm_nn_install.sh
@@ -78,6 +78,10 @@ download_lib() {
   if [ "$INSTALL_FLAG" -ne 0 ]; then
     return 0
   fi
+
+  if [ "$3" == "--" ]; then
+    3="--"
+  fi
   echo "Downloading $1..."
   local DIR="$BASEDIR/$1"
 
@@ -168,7 +172,7 @@ run_prog() {
   # Download TensorFlow, ArmNN, and FlatBuffers, then run generate_tensorflow_protobuf.sh
   disp_msg "Installing TensorFlow..."
 
-  download_lib "armnn" "https://github.com/Arm-software/armnn" ""
+  download_lib "armnn" "https://github.com/Arm-software/armnn"
 
   download_lib "tensorflow" "https://github.com/tensorflow/tensorflow.git"
   git checkout 590d6eef7e91a6a7392c8ffffb7b58f2e0c8bc6b
